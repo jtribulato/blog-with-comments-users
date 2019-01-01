664e0d228aa68efec7cd12a86d1bffc6a386fa52
John Tribulato <jtribulato@gmail.com>
2018-12-31 23:58:35 -0800

    just another commit

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 'use strict';
 
 exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL 
-|| 'mongodb://localhost/blogTestDb';
+|| 'mongodb://localhost/blog-app';
 exports.PORT = process.env.PORT || 8080;
